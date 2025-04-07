f2468bdc0d9984293be9ff1391008ce3f65825f0
Dany Paredes <danywalls4@gmail.com>
2025-04-07 15:20:27 +0200

    feat: 🎸 add loaders with theme

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 
 import { PonyButtonComponent } from './components/pony-button/pony-button.component';
+import {KENDO_BUTTON} from '@progress/kendo-angular-buttons';
+import {KENDO_INDICATORS} from '@progress/kendo-angular-indicators';
 
 @Component({
   selector: 'app-root',
-  imports: [PonyButtonComponent],
+  imports: [PonyButtonComponent, KENDO_BUTTON, KENDO_INDICATORS],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
